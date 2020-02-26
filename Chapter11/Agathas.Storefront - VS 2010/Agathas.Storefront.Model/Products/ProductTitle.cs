949a3702d53d0944c7a57011104bbd5415e68ebe
Adrian Dinu <adrian.dinu@outlook.com>
2020-02-26 16:00:54 +0200

    Resources of the Professional ASP.NET Design Patterns book.

---
+++ Chapter11/Agathas.Storefront - VS 2010/Agathas.Storefront.Model/Products/ProductTitle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Agathas.Storefront.Infrastructure.Domain;
+using Agathas.Storefront.Model.Categories;
+
+namespace Agathas.Storefront.Model.Products
+{
+    public class ProductTitle : EntityBase<int>, IAggregateRoot
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Brand Brand { get; set; }
+        public Category Category { get; set; }
+        public ProductColor Color { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
